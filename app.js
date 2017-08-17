1685d27afe9137074166d93c5eeb3c3a7a679e23
Your name <Mahima Subramanian>
2017-08-17 16:34:48 -0500

    installed session

--- app.js
+++ app.js
@@ -3,11 +3,18 @@
 const express = require('express');
 const mustacheExpress = require('mustache-express');
 const bodyParser = require('body-parser');
+const session = require('express-session');
 const app = express ();
 
 app.engine('mustache', mustacheExpress())
 app.set('views', './views')
 app.set('view engine', 'mustache')
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}))
 
 app.use(bodyParser.urlencoded({extended: false }));
 app.use(function (req,res,next) {
@@ -16,7 +23,7 @@ app.use(function (req,res,next) {
 })
 
 app.get('/', function (req, res) {
-  res.render('login')
+  res.render('index')
 
 })
 
