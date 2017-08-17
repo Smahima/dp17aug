15341347f90b4dbfcbefe378f131676ce748caf4
Your name <Mahima Subramanian>
2017-08-17 16:16:12 -0500

    posted form to login

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
-console.log('hello');
+
 
 const express = require('express');
 const mustacheExpress = require('mustache-express');
+const bodyParser = require('body-parser');
 const app = express ();
 
 app.engine('mustache', mustacheExpress())
 app.set('views', './views')
 app.set('view engine', 'mustache')
 
+app.use(bodyParser.urlencoded({extended: false }));
 
 app.get('/', function (req, res) {
   res.render('login')
 
 })
+
+app.post('/login', function (req, res) {
+  console.log('username is ' + req.body.username);
+  console.log('password is ' + req.body.password);
+  res.render('index')
+})
+
 app.listen(3000, function () {
   console.log('Ciao');
 })
