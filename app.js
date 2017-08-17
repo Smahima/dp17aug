633775dbaa22110b92bb02d835a343029ef3725a
Your name <Mahima Subramanian>
2017-08-17 16:01:15 -0500

    added login page

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 console.log('hello');
+
+const express = require('express');
+const mustacheExpress = require('mustache-express');
+const app = express ();
+
+app.engine('mustache', mustacheExpress())
+app.set('views', './views')
+app.set('view engine', 'mustache')
+
+
+app.get('/', function (req, res) {
+  res.render('login')
+
+})
+app.listen(3000, function () {
+  console.log('Ciao');
+})
