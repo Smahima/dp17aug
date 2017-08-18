e3eb36f1d10a41ea980b35f480acb414f82e8a29
Your name <Mahima Subramanian>
2017-08-17 19:13:19 -0500

    need to fix errors in this, but commiting

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const mustacheExpress = require('mustache-express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const app = express ();
+const userData = [
+  {user: 'Meena', pass: 'singam'},
+  {user: 'Johnny5', pass:'circuit'},
+  {user: 'admin', pass: 'admin'}
+];
+console.log(userData);
 
 app.engine('mustache', mustacheExpress())
 app.set('views', './views')
@@ -16,11 +22,26 @@ app.use(session({
   saveUninitialized: true
 }))
 
+
 app.use(bodyParser.urlencoded({extended: false }));
 app.use(function (req,res,next) {
   console.log('in interceptor');
-  next()
+function checkLogin(username, password) {
+    if (req.session == 'login') {
+      next()
+    } else if (!== res.session) {
+      res.render('login')
+    } else {
+      next()
+    }
+  }
 })
+  // / if req.url == '/login'
+  //     //     next()
+      // else if !req.session.login
+      //     res.render('login')
+      // else
+      //     next()
 
 app.get('/', function (req, res) {
   res.render('index')
@@ -30,6 +51,24 @@ app.get('/', function (req, res) {
 app.post('/login', function (req, res) {
   console.log('username is ' + req.body.username);
   console.log('password is ' + req.body.password);
+    for (let i = 0; i < data.length; i++) {
+      if (req.body.username === [i].data.username, req.body.password === [i].data.username) {
+          res.render('index')
+      }
+      else {
+        res.render('login')
+      }
+    }
+    })
+
+  // loop through array to find username and password
+   // if you find a match then set req.session.login =true
+   // req.session.user = req.body.username
+   // res.render('index')
+
+   // if not then render login with an error message
+   // res.render('login')
+
   res.render('index')
 })
 
